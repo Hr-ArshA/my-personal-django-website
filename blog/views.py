3161aab73f1c326b751903f5826285e46c1be351
Hr-ArshA <a_sh1379@yahoo.cpm>
2021-01-27 11:41:41 +0330

    add blog view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from blog.models import Post
+
 
 # Create your views here.
+
+
+class Blog(ListView):
+    queryset = Post.objects.all()
