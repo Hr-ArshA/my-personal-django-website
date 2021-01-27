a00db33636a8b88dedd72a409cbcd6f7b4e7cf7b
Hr-ArshA <a_sh1379@yahoo.cpm>
2021-01-27 17:07:27 +0330

    add commen app urls

--- conf/urls.py
+++ conf/urls.py
@@ -21,7 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('admin/', admin.site.urls),
+    path('comment/', include('comment.urls')),
 
 ]
 
