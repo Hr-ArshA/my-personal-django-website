58c6fb9cb8110b20e294b079f6cc9acb96e8534c
Hr-ArshA <a_sh1379@yahoo.cpm>
2021-01-30 20:54:18 +0330

    add postadmin

--- blog/admin.py
+++ blog/admin.py
@@ -4,5 +4,16 @@ from blog.models import Post, Category
 # Register your models here.
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('Title','slug','jpublish', 'Author','Status','category_to_str')
+    list_filter = ('Publish','Status')
+    search_fields = ('Title','Description')
+    prepopulated_fields = {'slug':('Title',)}
+    ordering = ['-Publish']
+
+    def category_to_str(self, obj):
+        return '، '.join([Category.Title for Category in obj.category_published()])
+
+
+admin.site.register(Post,PostAdmin)
 admin.site.register(Category)
