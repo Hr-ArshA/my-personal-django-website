9a93e88cfc82f4a3689372a1d646fa6ca3a7864e
Hr-ArshA <a_sh1379@yahoo.cpm>
2021-01-30 17:29:58 +0330

    add sidebar view

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-from blog.models import Post
+from blog.models import Post, Category
 
 
 # Create your views here.
 
+def sidebar(request, *args, **kwargs):
+    context = {
+        'category': Category.objects.filter(Status=True),
+        'Post': Post.objects.filter(Status=True),
+    }
+    return render(request, 'blog/sidebar.html', context)
+
 
 class Blog(ListView):
-    queryset = Post.objects.filter(Status=False)
+    queryset = Post.objects.filter(Status=True)
     paginate_by = 8
 
 class blog_detail(DetailView):
-    queryset = Post.objects.filter(Status=False)
+    queryset = Post.objects.filter(Status=True)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         return context
+
+
+
