b72d7843e1502e5a64757cfa68860e51dc58e0d3
Hr-ArshA <a_sh1379@yahoo.cpm>
2021-01-30 17:54:41 +0330

    add aoutor url

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from blog.views import Blog, blog_detail, sidebar
+from blog.views import Blog, blog_detail, sidebar, category_list, Author_list
 
 urlpatterns = [
     path('', Blog.as_view(), name='blog'),
     path('<slug:slug>/', blog_detail.as_view(), name='blog_detail'),
     path('sidebar', sidebar, name='sidebar'),
     path('category/<slug:slug>', category_list.as_view(), name='category'),
+    path('author/<slug:username>', Author_list.as_view(), name='author'),
 ]
