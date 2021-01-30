fdf200430e25ba220867534e25485627e43fcc6d
Hr-ArshA <a_sh1379@yahoo.cpm>
2021-01-30 19:22:08 +0330

    add footer

--- conf/views.py
+++ conf/views.py
@@ -10,3 +10,9 @@ def header(request, *args, **kwargs):
     }
     return render(request, 'blog/shared/header.html', context)
 
+def footer(request, *args, **kwargs):
+    context = {
+        'category': Category.objects.filter(Status=True),
+        'Post': Post.objects.filter(Status=True),
+    }
+    return render(request, 'blog/shared/footer.html', context)
