7a0b6d9b11d60b15c4d15527b086a2b66cf60a57
Hr-ArshA <a_sh1379@yahoo.cpm>
2021-02-08 14:45:28 +0330

    add and edit User

--- Account/forms.py
+++ Account/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
+from Account.models import User
 from django.core import validators
 
 
