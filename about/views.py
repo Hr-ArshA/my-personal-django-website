6dc7e6b30d2c9f67410cd89685a8bc1f5e2e4fb8
Hr-ArshA <a_sh1379@yahoo.com>
2021-02-15 13:21:55 +0330

    add about view

--- about/views.py
+++ about/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
 
 # Create your views here.
+from about.models import About
+
+
+class about_us(DetailView):
+    queryset = About.objects.filter(slug='about')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        return context
